c04f57ae43b76787fcbe44403fb05abf507d9303
Nikita Bosov <neckbosov@gmail.com>
2021-11-22 02:25:42 +0300

    Add main program

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::fs::File;
+use std::path::PathBuf;
+use structopt::StructOpt;
+use trivial_graph::{BfsVisitor, Graph, GraphVertex, GraphVisitor, VisitOrder};
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "graph-viewer")]
+struct Opt {
+    /// Path to graph
+    #[structopt(short, long, parse(from_os_str))]
+    file: PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opt: Opt = Opt::from_args();
+    let mut file = File::open(opt.file).expect("failed to open graph file");
+    let graph: Graph<String> = Graph::from_reader(&mut file).unwrap();
+    let vertex_printer = |v: &GraphVertex<String>| {
+        println!("Vertex: {}", v.id);
+        let neighbours: Vec<_> = graph.get_neighbours(v.id).unwrap().into_iter().map(|id| id.to_string()).collect();
+        println!("Neighbours: {}", neighbours.join(" "));
+        println!("Value: {}", v.value);
+    };
+    let mut visitor = BfsVisitor::new(&graph);
+    visitor.visit_all(VisitOrder::TopologicalSort, vertex_printer);
 }
